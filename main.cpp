b9abe3892b2f3f7291190b1c514c55f59ee4c85d
bhuiyanmohammedabubakarstudente-stack <bhuiyan.mohammedabubakar.studente@itispaleocapa.it>
2026-05-08 10:35:41 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include <string>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+	struct numerazione{
+		string tipovia;
+		string nomevia;
+		string numero;
+		string subalterno;
+		string cap;
+		int istat;
+		double latitudine;
+		double longitudine;
+		string location;
+		
+	};
+	numerazione x;
+	
+	return 0;
+}
