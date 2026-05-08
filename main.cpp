b4b4f4d2511280d09285f4e73967ca88070f0bfd
bhuiyanmohammedabubakarstudente-stack <bhuiyan.mohammedabubakar.studente@itispaleocapa.it>
2026-05-08 11:31:01 +0200

    funzione visualizza dati

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@ using namespace std;
 #include <fstream>
 #include <string>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-	struct numerazionecivica{
+	struct location{
+		double x;
+		double y;
+	};
+	struct numcivica{
 		string tipovia;
 		string nomevia;
 		string numero;
@@ -15,16 +19,37 @@ using namespace std;
 		location z;
 		
 	};
-	struct location{
-		string x;
-		string y;
-	};
+	
+string visualizzadati(numcivica arr[],int dim ){
+	string s;
+	for(int i=0;i<dim;i++){
+		s+=arr[i].tipovia+"\t";
+		s+=arr[i].nomevia+"\t";
+		s+=arr[i].numero+"\t";
+		s+=arr[i].subalterno+"\t";
+		s+=arr[i].cap+"\t";
+		s+=arr[i].istat+"\t";
+		s+=to_string(arr[i].latitudine)+"\n";
+		s+=to_string(arr[i].longitudine)+"\n";
+		s+=to_string(arr[i].z.x)+"\t";
+		s+=to_string(arr[i].z.y)+"\t";
+	}
+	return s;
+	
+}
 
 // lettura riga per riga il file e per ogni riga estraggo i campi e li salvo nel campo giusto.
-caricadati(numerazionecivica arr[],string nome){
+void caricadati(numcivica arr[100],string nome){
 ifstream leggi("dati.csv");
-if(leggi.is_open){
-
+if(leggi.is_open()){
+string riga;
+while(getline(leggi,riga)){
+	
+}
+leggi.close();
+}
+else{
+	cout<<"file non è stato aperto correttamente"<<endl;
 }
 }
 
@@ -50,8 +75,8 @@ int main(int argc, char** argv)
 		string x;
 		string y;
 	};*/
-	numerazionecivica dati[];
+	numcivica dati[100];
 	
-	
+	caricadati(dati,"dati.csv");
 	return 0;
 }
