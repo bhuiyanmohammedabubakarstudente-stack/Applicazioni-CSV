aa3ca14f6a508091f9e955fbad2210d69cdc2098
bhuiyanmohammedabubakarstudente-stack <bhuiyan.mohammedabubakar.studente@itispaleocapa.it>
2026-05-08 10:53:18 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@ using namespace std;
 #include <fstream>
 #include <string>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char** argv) 
-{
 	struct numerazionecivica{
 		string tipovia;
 		string nomevia;
@@ -22,6 +19,37 @@ int main(int argc, char** argv)
 		string x;
 		string y;
 	};
+
+// lettura riga per riga il file e per ogni riga estraggo i campi e li salvo nel campo giusto.
+caricadati(numerazionecivica arr[],string nome){
+ifstream leggi("dati.csv");
+if(leggi.is_open){
+
+}
+}
+
+
+
+
+int main(int argc, char** argv) 
+{
+	/*
+	struct numerazionecivica{
+		string tipovia;
+		string nomevia;
+		string numero;
+		string subalterno;
+		string cap;
+		string istat;
+		double latitudine;
+		double longitudine;
+		location z;
+		
+	};
+	struct location{
+		string x;
+		string y;
+	};*/
 	numerazionecivica dati[];
 	
 	
